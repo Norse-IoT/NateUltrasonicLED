78150b52159caced45f7283176b433264de8a631
George Paredes <119899081+paredesg27@users.noreply.github.com>
2023-01-30 18:23:57 -0500

    Update Ultrasonnic LED.ino

--- Ultrasonnic LED.ino
+++ Ultrasonnic LED.ino
@@ -1,115 +1,149 @@
-#define TRIG_PIN 32 // ESP32 pin GIOP23 connected to Ultrasonic Sensor's TRIG pin
+#define TRIG_PIN 32  // ESP32 pin GIOP23 connected to Ultrasonic Sensor's TRIG pin
 #define TRIG_PIN2 15
-#define ECHO_PIN 33 // ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
+#define ECHO_PIN 33
+  // ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
 #define ECHO_PIN2 2
 #define SOUND_SPEED 0.017
 #define LED_PIN 25
 #define LED_PIN2 27
-//#define SDA 21 // Sending and recieving data for LCD display (Send Data with Acknowledge)
-//#define SCL 22 // Used for the Clock signal (Serial Clock Line)
-#define DIS_THRES 125.00 
-#define DIS_THRES2 125.00 // centimeters
-//#define SCREEN_WIDTH 128 // OLED display width, in pixels
-//#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+//#define SDA 21          // Sending and recieving data for LCD display (Send Data with
+                // Acknowledge)
+//#define SCL 22  // Used for the Clock signal (Serial Clock Line)
+#define DIS_THRES 25
+#define DIS_THRES2 125.00  // centimeters
+/*#define SCREEN_WIDTH 128   // OLED display width, in pixels
+#define SCREEN_HEIGHT 64   // OLED display height, in pixels*/
 int numPeople = 0;
 
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
 #include <iostream>
 #include <string>
-
-float duration_us, distance_cm;
-float duration_us2, distance_cm2;
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 //Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 2);
 
+enum Direction {
+    EnterRoom,
+    ExitRoom,
+    Neither,
+};
+
+class Sensor {
+   public:
+    Sensor(int pin) {
+        this->pin = pin;
+        this->hasTriggered = false;
+        this->lastTriggered = 0;
+    }
+
+    static bool sensorHasTriggered(const int pin) {
+        // generate 10-microsecond pulse to TRIG pin
+        digitalWrite(TRIG_PIN, HIGH);
+        delayMicroseconds(1);
+        digitalWrite(TRIG_PIN, LOW);
+
+        // measure duration of pulse from ECHO pin
+        float duration_us = pulseIn(ECHO_PIN, HIGH);
+
+        // calculate the distance
+        float distance_cm = SOUND_SPEED * duration_us;
+
+        // print the value to Serial Monitor
+       /* Serial.print("distance for pin ");
+        Serial.print(pin);
+        Serial.print(": ");
+        Serial.print(distance_cm);
+        Serial.print(" cm  ");
+        Serial.println(DIS_THRES);
+        */
+        return distance_cm < DIS_THRES;
+    }
+
+    void detect() {
+        if (sensorHasTriggered(this->pin)) {
+            lastTriggered = millis();
+            hasTriggered = true;
+        }
+    }
+
+    Direction compare(Sensor& other) {
+        if (this->hasTriggered && other.hasTriggered) {
+            this->hasTriggered = false;  // do not detect next time
+            other.hasTriggered = false;
+
+            if (this->lastTriggered > other.lastTriggered) {
+                return Direction::EnterRoom;
+            } else {
+                return Direction::ExitRoom;
+            }
+        } else {
+            return Direction::Neither;
+        }
+    }
+
+    bool hasTriggered;
+    unsigned long lastTriggered;
+    int pin;
+};
+
 void setup() {
-  // begin serial port
-  Serial.begin (115200);
+    // begin serial port
+    Serial.begin(115200);
 
-  // configure the trigger pin to output mode
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(TRIG_PIN2, OUTPUT);
-  // configure the echo pin to input mode
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(ECHO_PIN2, INPUT);
+    // configure the trigger pin to output mode
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(TRIG_PIN2, OUTPUT);
+    // configure the echo pin to input mode
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(ECHO_PIN2, INPUT);
 
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(LED_PIN2, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_PIN2, OUTPUT);
 
-  /*f(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x32
-    Serial.println("SSD1306 alloction failed");
-    for(;;); // Don't proceed, loop forever
-  }
-  delay(2000);
-  display.clearDisplay();
+    /*if (!display.begin(SSD1306_SWITCHCAPVCC,
+                       0x3C)) {  // Address 0x3D for 128x32
+        Serial.println("SSD1306 alloction failed");
+        for (;;)
+            ;  // Don't proceed, loop forever
+    }
+    delay(2000);
+    display.clearDisplay();
 
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
     // display static text
 
-  display.println("Number of People: \n"); 
-  display.println(numPeople);
-  display.display();*/
+    display.println("Number of People: \n");
+    display.println(numPeople);
+    display.display();*/
+
+    Sensor sensor1(LED_PIN);
+    Sensor sensor2(LED_PIN2);
+
+    while (true) {  // loop
+        sensor1.detect();
+        sensor2.detect();
+        Serial.println("Number of People: \n");
+        Serial.println(numPeople);
+
+        switch (sensor1.compare(sensor2)) {
+        EnterRoom:
+            Serial.println("EnterRoom");
+            numPeople++;
+            break;
+        ExitRoom:
+            Serial.println("ExitRoom");
+            numPeople--;
+            break;
+        Neither:
+           // Serial.println("Neither");
+            break;
+        }
+    }
 }
 
-void loop() {
-
-  char first[] = "";  
-  
-  // generate 10-microsecond pulse to TRIG pin
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(1);
-  digitalWrite(TRIG_PIN, LOW);
-
-  // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(ECHO_PIN, HIGH);
-
-  // calculate the distance
-  distance_cm = SOUND_SPEED * duration_us;
-  
-  // generate 10-microsecond pulse to TRIG pin 2
-  digitalWrite(TRIG_PIN2, HIGH);
-  delayMicroseconds(1);
-  digitalWrite(TRIG_PIN2, LOW);
-
-  // measure duration of pulse from ECHO pin 2
-  duration_us2 = pulseIn(ECHO_PIN2, HIGH);
-
-  // calculate the second distance
-  distance_cm2 = SOUND_SPEED * duration_us2;
-
-  
-  if (distance_cm < DIS_THRES){
-    digitalWrite(LED_PIN, LOW); //turn on LED 1*Illogical statement*      
-  }
-  else {
-    digitalWrite(LED_PIN, HIGH);
-  }
-  //if else {
-    
- // }
-  if (distance_cm2 < DIS_THRES2) {
-    digitalWrite(LED_PIN2, LOW);  //trun on LED 2
-  }
-  else {
-    digitalWrite(LED_PIN2, HIGH);
-  }  
-  
-
-  // print the value to Serial Monitor
-  Serial.print("distance1: ");
-  Serial.print(distance_cm);
-  Serial.print(" cm  ");
-  Serial.println(DIS_THRES);
-  // print the value to Serial Monitor
-  Serial.print("distance2: ");
-  Serial.print(distance_cm2);
-  Serial.print(" cm  ");
-  Serial.println(DIS_THRES2);
-  Serial.println("-----------------------");
-  delay(50);
-}
+void loop() {}
