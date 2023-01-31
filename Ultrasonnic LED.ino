bae2fd1a7bada46423a1850fd90023dedc543e33
Zack Sargent <46602241+zsarge@users.noreply.github.com>
2023-01-30 19:34:15 -0500

    [WIP] fix some bugs
    still reads 0.0 from sensors

--- Ultrasonnic LED.ino
+++ Ultrasonnic LED.ino
@@ -1,13 +1,13 @@
 #define TRIG_PIN 32  // ESP32 pin GIOP23 connected to Ultrasonic Sensor's TRIG pin
 #define TRIG_PIN2 15
 #define ECHO_PIN 33
-  // ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
+// ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
 #define ECHO_PIN2 2
 #define SOUND_SPEED 0.017
 #define LED_PIN 25
 #define LED_PIN2 27
 //#define SDA 21          // Sending and recieving data for LCD display (Send Data with
-                // Acknowledge)
+// Acknowledge)
 //#define SCL 22  // Used for the Clock signal (Serial Clock Line)
 #define DIS_THRES 25
 #define DIS_THRES2 125.00  // centimeters
@@ -26,84 +26,88 @@ int numPeople = 0;
 //Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 2);
 
 enum Direction {
-    EnterRoom,
-    ExitRoom,
-    Neither,
+  EnterRoom,
+  ExitRoom,
+  Neither,
 };
 
 class Sensor {
-   public:
-    Sensor(int pin) {
-        this->pin = pin;
-        this->hasTriggered = false;
-        this->lastTriggered = 0;
+public:
+  Sensor(int triggerPin, int echoPin) {
+    this->triggerPin = triggerPin;
+    this->echoPin = echoPin;
+    this->hasTriggered = false;
+    this->lastTriggered = 0;
+  }
+
+  bool sensorHasTriggered() {
+    // generate 10-microsecond pulse to TRIG pin
+    digitalWrite(this->triggerPin, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(this->triggerPin, LOW);
+
+    // measure duration of pulse from ECHO pin
+    float duration_us = pulseIn(this->echoPin, HIGH);
+
+    // calculate the distance
+    float distance_cm = SOUND_SPEED * duration_us;
+
+    bool trigger = distance_cm < DIS_THRES;
+
+    // print the value to Serial Monitor
+    if (trigger) {
+      Serial.print(this->triggerPin);
+      Serial.print(" has triggered: ");
+      Serial.print(distance_cm);
+      Serial.print(" cm < ");
+      Serial.println(DIS_THRES);
     }
+    return trigger;
+  }
 
-    static bool sensorHasTriggered(const int pin) {
-        // generate 10-microsecond pulse to TRIG pin
-        digitalWrite(TRIG_PIN, HIGH);
-        delayMicroseconds(1);
-        digitalWrite(TRIG_PIN, LOW);
-
-        // measure duration of pulse from ECHO pin
-        float duration_us = pulseIn(ECHO_PIN, HIGH);
-
-        // calculate the distance
-        float distance_cm = SOUND_SPEED * duration_us;
-
-        // print the value to Serial Monitor
-       /* Serial.print("distance for pin ");
-        Serial.print(pin);
-        Serial.print(": ");
-        Serial.print(distance_cm);
-        Serial.print(" cm  ");
-        Serial.println(DIS_THRES);
-        */
-        return distance_cm < DIS_THRES;
+  void detect() {
+    if (this->sensorHasTriggered()) {
+      lastTriggered = millis();
+      hasTriggered = true;
     }
+  }
 
-    void detect() {
-        if (sensorHasTriggered(this->pin)) {
-            lastTriggered = millis();
-            hasTriggered = true;
-        }
+  Direction compare(Sensor& other) {
+    if (this->hasTriggered && other.hasTriggered) {
+      this->hasTriggered = false;  // do not detect next time
+      other.hasTriggered = false;
+
+      if (this->lastTriggered > other.lastTriggered) {
+        return Direction::EnterRoom;
+      } else {
+        return Direction::ExitRoom;
+      }
+    } else {
+      return Direction::Neither;
     }
+  }
 
-    Direction compare(Sensor& other) {
-        if (this->hasTriggered && other.hasTriggered) {
-            this->hasTriggered = false;  // do not detect next time
-            other.hasTriggered = false;
-
-            if (this->lastTriggered > other.lastTriggered) {
-                return Direction::EnterRoom;
-            } else {
-                return Direction::ExitRoom;
-            }
-        } else {
-            return Direction::Neither;
-        }
-    }
-
-    bool hasTriggered;
-    unsigned long lastTriggered;
-    int pin;
+  bool hasTriggered;
+  unsigned long lastTriggered;
+  int triggerPin;
+  int echoPin;
 };
 
 void setup() {
-    // begin serial port
-    Serial.begin(115200);
+  // begin serial port
+  Serial.begin(9600);
 
-    // configure the trigger pin to output mode
-    pinMode(TRIG_PIN, OUTPUT);
-    pinMode(TRIG_PIN2, OUTPUT);
-    // configure the echo pin to input mode
-    pinMode(ECHO_PIN, INPUT);
-    pinMode(ECHO_PIN2, INPUT);
+  // configure the trigger pin to output mode
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(TRIG_PIN2, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(ECHO_PIN2, INPUT);
 
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
 
-    /*if (!display.begin(SSD1306_SWITCHCAPVCC,
+  /*if (!display.begin(SSD1306_SWITCHCAPVCC,
                        0x3C)) {  // Address 0x3D for 128x32
         Serial.println("SSD1306 alloction failed");
         for (;;)
@@ -121,29 +125,29 @@ void setup() {
     display.println(numPeople);
     display.display();*/
 
-    Sensor sensor1(LED_PIN);
-    Sensor sensor2(LED_PIN2);
+  Sensor sensor1(LED_PIN, ECHO_PIN);
+  Sensor sensor2(LED_PIN2, ECHO_PIN2);
 
-    while (true) {  // loop
-        sensor1.detect();
-        sensor2.detect();
-        Serial.println("Number of People: \n");
-        Serial.println(numPeople);
+  while (true) {  // loop
+    sensor1.detect();
+    sensor2.detect();
+    // Serial.println("Number of People: \n");
+    // Serial.println(numPeople);
 
-        switch (sensor1.compare(sensor2)) {
-        EnterRoom:
-            Serial.println("EnterRoom");
-            numPeople++;
-            break;
-        ExitRoom:
-            Serial.println("ExitRoom");
-            numPeople--;
-            break;
-        Neither:
-           // Serial.println("Neither");
-            break;
-        }
+    switch (sensor1.compare(sensor2)) {
+EnterRoom:
+      Serial.println("EnterRoom");
+      numPeople++;
+      break;
+ExitRoom:
+      Serial.println("ExitRoom");
+      numPeople--;
+      break;
+Neither:
+      // Serial.println("Neither");
+      break;
     }
+  }
 }
 
 void loop() {}
